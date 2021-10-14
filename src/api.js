8bec16d9b8c04ca7a8afbfc07691284b49a61582
afrahdev <afrahabdul@gmail.com>
2021-10-14 14:16:55 +0100

    ficed issue and more

--- src/api.js
+++ src/api.js
@@ -36,3 +36,11 @@ const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${newGames}`;
+//GAME DETAILS
+export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
+//Game ScreenShots
+export const gameScreenshotURL = (game_id) =>
+  `${base_url}games/${game_id}/screenshots`;
+//Searched game
+export const searchGameURL = (game_name) =>
+  `${base_url}games?search=${game_name}&page_size=9`;
